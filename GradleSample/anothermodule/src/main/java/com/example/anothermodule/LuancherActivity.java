620add8fb5ef097c02bc70df3499e5ee629e0215
jun <912833721@qq.com>
2023-03-28 14:50:26 +0800

    仿AGP以及引入auto-value，bean中自动帮我们补充setting/getting方法

---
+++ GradleSample/anothermodule/src/main/java/com/example/anothermodule/LuancherActivity.java
@@ -0,0 +1,13 @@
+package com.example.anothermodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LuancherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luancher);
+    }
+}
